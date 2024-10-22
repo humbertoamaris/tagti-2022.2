d14ad38dac71f0d7469f23f9ed10eaeb696e06d6
Humberto Amaris <humberto.lima@aluno.ifsertao-pe.edu.br>
2024-10-21 21:01:51 -0300

    atualização

--- microblog/app.py
+++ microblog/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Olá Mundo!'
+    return render_template("index.html")
+
+@app.route("/contato")
+def contato():
+    return("humberto.lima@aluno.ifsertao-pe.edu.br")
 
 if __name__ == '__main__':
     app.run()
