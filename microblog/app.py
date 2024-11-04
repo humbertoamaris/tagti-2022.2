144588d7769df4db85c674302244d1b45cf93ea3
Humberto Amaris <humberto.lima@aluno.ifsertao-pe.edu.br>
2024-11-04 20:24:52 -0300

    Update app.py

--- microblog/app.py
+++ microblog/app.py
@@ -9,7 +9,12 @@ def index():
 @app.route("/contato")
 def contato():
 
-    return render_template("contato.html", tel="(87) 988889898")
+    return render_template("contato.html", tel="(87) 988889898", email="humberto.lima@aluno.ifsertao-pe.edu.br")
+  
+@app.route("/dados")
+def dados(): 
+    return render_template("dados.ht", tel="(87) 988889898")
+
 
 if __name__ == '__main__':
     app.run()
